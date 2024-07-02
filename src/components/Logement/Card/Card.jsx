9f95be6901cfc81b7198e85076afdf6280cfc672
Dodge74-DRO <rogerdromard@gmail.com>
2024-07-02 19:21:08 +0200

    correction zoe link pour click lien vers page location/full

--- src/components/Logement/Card/Card.jsx
+++ src/components/Logement/Card/Card.jsx
@@ -5,14 +5,14 @@ import './card.scss'
 
 export default function Card({location}) {
    return (
+      <Link to={`/location/${location.id}`} key={location.id} >
       <figure className='card'>
          {/* création de lien URL si click sur image avec href=/location/location.id */}
          {/* ex /location/c67ab8a7c   l'ID sera récupéré dans l'URL par compost FULL */}
-         <Link to={`/location/${location.id}`} key={location.id} >
             <img className='img' src={location.cover} alt={location.title}/>
-            <div className='gradient'></div>          
-            <figcaption className='title'>{location.title}</figcaption>
-         </Link>
+         <div className='gradient'></div>          
+         <figcaption className='title'>{location.title}</figcaption>
       </figure>
+      </Link>
    )
 }
